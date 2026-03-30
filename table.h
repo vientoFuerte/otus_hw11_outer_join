608f02d0ef9469c1539edc76ac55006522715e2e
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-03-30 22:19:12 +0300

    Update table.h
    форматирование

--- table.h
+++ table.h
@@ -1,16 +1,13 @@
 #pragma once
 
+#include <map>
 #include <vector>
 #include <string>
 #include <iostream>
-#include <fstream>
+
 #include "debug_settings.h"
-#include <map>
 
 namespace table {
 
-
-
-
-  void receive(const char* data, std::size_t size);
+  std::string receive(const char* data, std::size_t size);
 }
