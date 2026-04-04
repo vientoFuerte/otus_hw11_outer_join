3f54a2342a8e271a478a9033ade36b7fd1fc61bc
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-04 18:18:24 +0300

    Update table.cpp
    Убраны повторяющиеся действия в receive()

--- table.cpp
+++ table.cpp
@@ -28,123 +28,117 @@ void printTables()
 }
   
 std::string parseCommand(const std::string& input) {
-  std::stringstream ss(input);
-  std::string command;
-  ss >> command; // Считываем первое слово (команду)
+    std::stringstream ss(input);
+    std::string command;
+    ss >> command; // Считываем первое слово (команду)
 
-  DLOG("parseCommand: operation=" << input << std::endl;)
+    DLOG("parseCommand: operation=" << input << std::endl;)
 
-  if (input.empty()) return "ERR ...\n";
-  
-  if (command == "INSERT") {
-      std::string table, name;
-      int id;
-      // Ожидаем: INSERT table_name id name
-      if (ss >> table >> id >> name) {
-          if (table == "A") {
-              // Проверяем существование ключа
-              if (tableA.find(id) != tableA.end()) {
-                  return "ERR duplicate " + std::to_string(id) + "\n";
-              }
-              else
-              {
-                  tableA[id] = name;
-                  return "OK\n";
-              }
+    if (input.empty()) return "ERR ...\n";
+    
+    if (command == "INSERT") {
+        std::string table, name;
+        int id;
+        // Ожидаем: INSERT table_name id name
+        if (ss >> table >> id >> name) {
+            if (table == "A") {
+                // Проверяем существование ключа
+                if (tableA.find(id) != tableA.end()) {
+                    return "ERR duplicate " + std::to_string(id) + "\n";
+                }
+                else
+                {
+                    tableA[id] = name;
+                    return "OK\n";
+                }
 
-          } 
-          else if (table == "B") {
-              // Проверяем существование ключа
-              if (tableB.find(id) != tableB.end()) {
-                  return "ERR duplicate " + std::to_string(id) + "\n";
-              }
-              else
-              {
-                  tableB[id] = name;
-                  return "OK\n";       
-              }
-          }
-          else {
-              return "ERR Unknown table: " + table + "\n";
-          }
-          
-      }
-  } 
-  else if (command == "TRUNCATE") {
-      std::string table;
-      // Ожидаем: TRUNCATE table_name
-      if (ss >> table) {
-          if (table == "A") {
-              tableA.clear();
-              return "OK\n";
-          } else if (table == "B") {
-              tableB.clear();     
-              return "OK\n";  
-          }
-          else {
-              return "ERR Unknown table: " + table + "\n";
-          }
-      }
-  } 
-  else if (command == "INTERSECTION") {
-      std::ostringstream response;
-      
-      for(const auto &pair: tableA)
-      {
-          auto it = tableB.find(pair.first);
-          if(it!= tableB.end())
-          {
-            // Формат: id,valueA,valueB
-            response << pair.first << "," << pair.second << "," << it->second << "\n";
-          }
-      }
-      response << "OK\n";
-      return response.str();
-      
-  } 
-  else if (command == "SYMMETRIC_DIFFERENCE") {        
-      std::ostringstream response;
-      for(const auto &pair: tableA)
-      {
-          auto it = tableB.find(pair.first);
-          if(it == tableB.end())
-          {
-              // Формат: id,valueA, (пустое значение для B)
-              response << pair.first << "," << pair.second << ",\n";
-          }
-      }
-      for(const auto &pair: tableB)
-      {
-          auto it = tableA.find(pair.first);
-          if(it == tableA.end())
-          {
-             response << pair.first << ",," << pair.second << "\n";
-          }
-      }
-      response << "OK\n"; 
-      return response.str();
-      
-  } 
-  else {
-      return "ERR Unknown command: " + command + "\n";  // Возвращаем ошибку
-  }
-  return "ERR Internal error\n";
+            } 
+            else if (table == "B") {
+                // Проверяем существование ключа
+                if (tableB.find(id) != tableB.end()) {
+                    return "ERR duplicate " + std::to_string(id) + "\n";
+                }
+                else
+                {
+                    tableB[id] = name;
+                    return "OK\n";       
+                }
+
+            }
+            else {
+                return "ERR Unknown table: " + table + "\n";
+            }
+            
+        }
+    } 
+    else if (command == "TRUNCATE") {
+        std::string table;
+        // Ожидаем: TRUNCATE table_name
+        if (ss >> table) {
+            if (table == "A") {
+                tableA.clear();
+                return "OK\n";
+            } else if (table == "B") {
+                tableB.clear();     
+                return "OK\n";  
+            }
+            else {
+                return "ERR Unknown table: " + table + "\n";
+            }
+        }
+        // обработка ошибки, если не удалось прочитать таблицу
+        return "ERR TRUNCATE requires table name\n";
+    } 
+    else if (command == "INTERSECTION") {
+        std::ostringstream response;
+        
+        for(const auto &pair: tableA)
+        {
+            auto it = tableB.find(pair.first);
+            if(it!= tableB.end())
+            {
+              // Формат: id,valueA,valueB
+              response << pair.first << "," << pair.second << "," << it->second << "\n";
+            }
+        }
+        response << "OK\n";
+        return response.str();
+        
+    } 
+    else if (command == "SYMMETRIC_DIFFERENCE") {        
+        std::ostringstream response;
+        for(const auto &pair: tableA)
+        {
+            auto it = tableB.find(pair.first);
+            if(it == tableB.end())
+            {
+                // Формат: id,valueA, (пустое значение для B)
+                response << pair.first << "," << pair.second << ",\n";
+            }
+        }
+        for(const auto &pair: tableB)
+        {
+            auto it = tableA.find(pair.first);
+            if(it == tableA.end())
+            {
+               response << pair.first << ",," << pair.second << "\n";
+            }
+        }
+        response << "OK\n"; 
+        return response.str();
+        
+    } 
+    else {
+        return "ERR Unknown command: " + command + "\n";  // Возвращаем ошибку
+    }
+    
+    return "ERR Internal error\n";
 }
 
 
-std::string receive(const char* data, std::size_t size) {
+std::string receive(const std::string& data) {
 
-    //  копируем в строку size символов из data
-    std::string input(data, size);
-    //чтобы работать как с входным потоком
-    std::istringstream stream(input);
-    std::string line;
-    std::string response;
-    
-    while (std::getline(stream, line))  {
-      response = parseCommand(line);
-    }
-    return response;
+    return parseCommand(data);
 }
 
 } // namespace table 
